23cbdfcd35fa020663f7cd7313fa27cca5f2183f
ntt <1914866205@qq.com>
2020-11-15 20:43:54 +0800

    redis配置

--- soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
+++ soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
@@ -2,7 +2,9 @@ package com.soft1851.user.controller;
 
 import com.soft1851.api.controller.user.HelloControllerApi;
 import com.soft1851.common.result.GraceResult;
+import com.soft1851.common.utils.RedisOperator;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class HelloController implements HelloControllerApi {
+
+    @Autowired
+    private RedisOperator redis;
+
     @Override
     public Object hello() {
         log.info("info:hello");
@@ -24,5 +30,9 @@ public class HelloController implements HelloControllerApi {
         return GraceResult.ok("hello");
     }
 
-    @GetMapping("/")
+    @GetMapping("/redis")
+    public GraceResult redis() {
+        redis.set("age", "20");
+        return GraceResult.ok(redis.get("age"));
+    }
 }
