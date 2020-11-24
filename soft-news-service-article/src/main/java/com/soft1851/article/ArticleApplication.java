6bcc4645fe9d0478d686110b89f95ac8aa322664
ntt <1914866205@qq.com>
2020-11-24 15:03:11 +0800

    文章服务模块准备

---
+++ soft-news-service-article/src/main/java/com/soft1851/article/ArticleApplication.java
@@ -0,0 +1,18 @@
+package com.soft1851.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 倪涛涛
+ * @version 1.0.0
+ * @ClassName ArticleApplication.java
+ * @Description TODO
+ * @createTime 2020年11月24日 15:02:00
+ */
+@SpringBootApplication
+public class ArticleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleApplication.class, args);
+    }
+}
